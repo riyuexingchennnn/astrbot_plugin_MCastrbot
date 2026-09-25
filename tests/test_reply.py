8227e37c8c4890fc22bbef52629ff86ad089420e
riyuexingchennnn <1287829233@qq.com>
2026-09-25 22:47:23 +0800

    fix:微调

--- tests/test_reply.py
+++ tests/test_reply.py
@@ -45,7 +45,8 @@ class SplitReplyTest(unittest.TestCase):
         self.assertEqual(schema["wake_keywords"]["default"], [])
         self.assertFalse(schema["public_auto_reply"]["default"])
         self.assertEqual(schema["mc_global_prompt"]["default"], "")
-        self.assertTrue(schema["mc_global_prompt"]["editor_mode"])
+        self.assertEqual(schema["public_auto_reply"]["description"], "允许回复 MC 公屏消息")
+        self.assertEqual(schema["mc_global_prompt"]["type"], "text")
         group = schema["segmentation"]
         self.assertEqual(group["type"], "object")
         self.assertEqual(group["condition"], {"segmented_reply": True})
